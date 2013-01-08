82474cec233704573c5076d97cc5af4a5f425283
Erik Svedäng <erik.svedang@gmail.com>
2013-01-08 23:52:02 +0100

    two players and state

--- src/gomoku/core.clj
+++ src/gomoku/core.clj
@@ -2,6 +2,26 @@
   (:require [quil.core :as q])
   (:gen-class))
 
+(def x-offset 15) ; in pixels
+
+(def y-offset 15) ; in pixels
+
+(def grid-size-x 20) ; nr of cells horizontal
+
+(def grid-size-y 15) ; nr of cells vertical
+
+(def cell-size 25) ; in pixels
+
+(def state (atom [{:player :a :pos [2 3]}
+                  {:player :b :pos [4 4]}]))
+
+(defn make-move [player x y]
+  (let [all-pos (map #(:pos %) @state)]
+    (doseq [pos all-pos]
+      (when (= pos [x y])
+        (throw (Exception. (str "Can't place move for player " player " on " [x y]))))))
+  (swap! state conj {:player player :pos [x y]}))
+
 (defn setup [])
 
 (defn scale [pixel-x pixel-y w h factor]
@@ -11,38 +31,47 @@
      (* w factor)
      (* h factor)]))
 
-(defn draw-shape [shape x y cell-size x-offset y-offset scale-factor]
+(defn draw-shape [shape x y scale-factor]
   (apply shape (scale (+ (* x cell-size) x-offset)
                       (+ (* y cell-size) y-offset)
                       cell-size
                       cell-size
                       scale-factor)))
 
-(defn draw-grid [x-dim y-dim cell-size x-offset y-offset]
+(defn draw-grid [x-dim y-dim]
   (q/stroke 20)
   (q/fill 255)
   (let [cells (for [x (range x-dim)
                     y (range y-dim)]
                 [x y])]
     (doseq [[x y] cells]
-      (draw-shape q/rect x y cell-size x-offset y-offset 1.0))))
+      (draw-shape q/rect x y 1.0))))
 
-(defn draw-shapes [shape cell-size x-offset y-offset]
-  )
+(defn draw-shapes [shape coords]
+  (doseq [[x y] coords]
+    (draw-shape shape x y 0.8)))
+
+(defn get-moves-for-player [state player]
+  (filter #(= player (:player %)) state))
 
 (defn draw []
   (q/ellipse-mode :corner)
   (q/background 210)
-  (draw-grid 15 10 30 20 20)
-  (q/fill 0)
-  (draw-shape q/ellipse 3 4 30 20 20 0.7))
+  (draw-grid grid-size-x grid-size-y)
+  (q/no-stroke)
+  (q/fill 255 0 0)
+  (draw-shapes q/ellipse (map #(:pos %) (get-moves-for-player @state :a)))
+  (q/fill 0 0 255)
+  (draw-shapes q/rect (map #(:pos %) (get-moves-for-player @state :b))))
 
 (defn create-window []
-  (q/defsketch gomoku
-               :title "Gomoku"
-               :size [800 600]
-               :setup setup
-               :draw draw))
+  (let [x-size (+ (* grid-size-x cell-size) (* 2 x-offset))
+        y-size (+ (* grid-size-y cell-size) (* 2 y-offset))]
+    (q/defsketch gomoku
+                 :title "Gomoku"
+                 :size [x-size y-size]
+                 :setup setup
+                 :draw draw)))
 
 (defn -main []
   (create-window))
